647aaff1bc933fac37c113e8e8a58e66153c502e
songrit0 <songrit208133@gmail.com>
2022-11-04 10:20:38 +0700

    405

--- Nuxt02/components/Tutorial.vue
+++ Nuxt02/components/Tutorial.vue
@@ -11,7 +11,7 @@
       -/562
         </h2>
         <p class="mt-3 text-gray-600">
-          We recommend you take a look at the s<a href="https://nuxtjs.org" target="_blank" class="button--doc text-green-500 hover:underline">Nuxt Documentation</a>, whether you are new or have previous experience with the framework.<br>
+          We recommend you take a look at the s151<a href="https://nuxtjs.org" target="_blank" class="button--doc text-green-500 hover:underline">Nuxt Documentation</a>, whether you are new or have previous experience with the framework.<br>
         </p>
         <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
           To get started, remove <code class="bg-gray-100 text-sm p-1 rounded border">components/Tutorial.vue</code> and start coding in <code class="bg-gray-100 text-sm p-1 rounded border">pages/index.vue</code>. Have fun!
